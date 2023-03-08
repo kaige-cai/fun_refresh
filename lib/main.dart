a5b6f0d38c51c07b73d02758e1f041bf6a7ec719
caikaige <caikaige.g@gmail.com>
2023-03-08 20:39:07 +0800

    new color

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: '趣刷'),
     );
   }
 }
@@ -39,6 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
@@ -63,4 +65,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
